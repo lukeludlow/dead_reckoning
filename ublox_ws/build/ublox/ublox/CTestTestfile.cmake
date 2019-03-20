2a244ce072fde1af3094bcb6fc9810ad4c3fe2c0
lukeludlow <lukeludlow@live.com>
2019-03-20 13:32:37 -0600

    build ublox gps node

---
+++ ublox_ws/build/ublox/ublox/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/luke/dead_reckoning/ublox_ws/src/ublox/ublox
+# Build directory: /home/luke/dead_reckoning/ublox_ws/build/ublox/ublox
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
